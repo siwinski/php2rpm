fd8edbc374dcd0c3d4b1ef6259cdb68fe3249f8b
Shawn Iwinski <siwinski@redhat.com>
2013-05-17 17:06:57 -0400

    Initial setup

---
+++ src/SAI/Php2Rpm.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of the php2rpm package.
+ *
+ * (c) Shawn Iwinski <shawn.iwinski@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SAI;
+
+/**
+ * @author Shawn Iwinski <shawn.iwinski@gmail.com>
+ */
+class Php2Rpm
+{
+
+    const VERSION = '1.0-dev';
+
+}
