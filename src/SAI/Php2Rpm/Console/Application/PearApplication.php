b050f0cf7b1dd04eca138832f68e710bfa8e6b88
Shawn Iwinski <siwinski@redhat.com>
2013-05-17 17:33:36 -0400

    [Console/Application/PearApplication] Initial implementation

---
+++ src/SAI/Php2Rpm/Console/Application/PearApplication.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file is part of the php2rpm package.
+ *
+ * (c) Shawn Iwinski <shawn.iwinski@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SAI\Php2Rpm\Console\Application;
+
+use SAI\Php2Rpm;
+use SAI\Php2Rpm\Console\Application\AbstractApplication;
+
+/**
+ * @author Shawn Iwinski <shawn.iwinski@gmail.com>
+ */
+class PearApplication extends AbstractApplication
+{
+
+    /**
+     * {@inheritDoc}
+     * @link http://patorjk.com/software/taag/#p=display&f=Standard&t=pear2rpm
+     */
+    protected static $logo = "                       ____
+  _ __   ___  __ _ _ _|___ \ _ __ _ __  _ __ ___
+ | '_ \ / _ \/ _` | '__|__) | '__| '_ \| '_ ` _ \
+ | |_) |  __/ (_| | |  / __/| |  | |_) | | | | | |
+ | .__/ \___|\__,_|_| |_____|_|  | .__/|_| |_| |_|
+ |_|                             |_|
+
+";
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        parent::__construct('pear2rpm', Php2Rpm::VERSION);
+    }
+
+}
