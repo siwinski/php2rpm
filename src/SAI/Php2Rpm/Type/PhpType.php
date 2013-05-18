825561eabf9766fdac82d25ffedb219bc0eaf525
Shawn Iwinski <siwinski@redhat.com>
2013-05-18 14:06:37 -0400

    [Type/*] Initial skeletons

---
+++ src/SAI/Php2Rpm/Type/PhpType.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of the php2rpm package.
+ *
+ * (c) Shawn Iwinski <shawn.iwinski@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SAI\Php2Rpm\Type;
+
+use SAI\Php2Rpm\Type\AbstractType;
+
+/**
+ * @author Shawn Iwinski <shawn.iwinski@gmail.com>
+ */
+class PhpType extends AbstractType
+{
+}
