a4cec02352bd8d7bb7f8ef4196cc1373038b867d
Shawn Iwinski <siwinski@redhat.com>
2013-06-05 16:26:54 -0400

    [Type/DrupalType] Initial implementation

--- src/SAI/Php2Rpm/Type/DrupalType.php
+++ src/SAI/Php2Rpm/Type/DrupalType.php
@@ -18,4 +18,54 @@ use SAI\Php2Rpm\Type\AbstractType;
  */
 class DrupalType extends AbstractType
 {
+
+    protected static $regex = '/^(\w+)(-\d(.x)?)?(-[0-9.]+)?$/';
+
+    public function isValid($project)
+    {
+        if (!preg_match(static::$regex, $project)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getName($project)
+    {
+        return $this->normalizeProject($project);
+    }
+
+    public function getVersion($project)
+    {
+        $apiVersion      = preg_replace('/^\w+-?/', '', $project);
+        $apiVersionParts = empty($apiVersion) ? array() : explode('-', $apiVersion);
+
+        if (empty($apiVersionParts)) {
+            return '*** TODO ***';
+        }
+
+        if (count($apiVersionParts) > 1) {
+            $api     = str_replace('.x', '', $apiVersionParts[0]);
+            $version = $apiVersionParts[1];
+        } elseif (false !== strpos($apiVersionParts[0], '.x')) {
+            $api     = str_replace('.x', '', $apiVersionParts[0]);
+            $version = null;
+        } else {
+            $api     = null;
+            $version = $apiVersionParts[0];
+        }
+
+        return '*version*';
+    }
+
+    public function getUrl($project)
+    {
+        return 'http://drupal.org/project/' . $this->normalizeProject($project);
+    }
+
+    public function getDownloadUrl($project)
+    {
+        return 'http://drupal.org/project/' . $this->normalizeProject($project);
+    }
+
 }
