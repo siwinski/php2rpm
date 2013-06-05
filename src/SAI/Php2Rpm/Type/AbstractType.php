d234e35e429bfcffe04ddf94d7711700b8a7c2d3
Shawn Iwinski <siwinski@redhat.com>
2013-06-05 16:26:35 -0400

    [Type/AbstractType] Initial implementation

--- src/SAI/Php2Rpm/Type/AbstractType.php
+++ src/SAI/Php2Rpm/Type/AbstractType.php
@@ -11,9 +11,85 @@
 
 namespace SAI\Php2Rpm\Type;
 
+use Symfony\Component\Console\Command\Command;
+use Guzzle\Http\Client;
+
 /**
  * @author Shawn Iwinski <shawn.iwinski@gmail.com>
  */
 abstract class AbstractType
 {
+    /**
+     *
+     */
+    protected $command;
+
+    /**
+     *
+     */
+    public function getCommand()
+    {
+        return $this->command;
+    }
+
+    /**
+     *
+     */
+    public function setCommand(Command $command)
+    {
+        $this->command = $command;
+        return $this;
+    }
+
+    /**
+     *
+     */
+    public function getApplication()
+    {
+        if (!isset($this->command)) {
+            throw new \RuntimeException('Type\'s command not set');
+        }
+
+        return $this->command->getApplication();
+    }
+
+    /**
+     *
+     * @param string $project
+     */
+    public function normalizeProject($project)
+    {
+        return $project;
+    }
+
+    /**
+     *
+     * @param string $project
+     */
+    abstract public function isValid($project);
+
+    /**
+     *
+     * @param string $project
+     */
+    abstract public function getName($project);
+
+    /**
+     *
+     * @param string $project
+     */
+    abstract public function getVersion($project);
+
+    /**
+     *
+     * @param string $project
+     */
+    abstract public function getUrl($project);
+
+    /**
+     *
+     * @param string $project
+     */
+    abstract public function getDownloadUrl($project);
+
 }
